db5c2db3b87f334a6c3aced1a7e70c063dfe8388
mrjoseph <trev_jos@hotmail.com>
2012-11-22 08:11:49 +0000

    added resize element

--- html/js/main.js
+++ html/js/main.js
@@ -1,1 +1,29 @@
+//Initlize objects
+var playground = playground || {};
+	playground.containers = playground.containers ||{};
+	playground.defaultVars = playground.defaultVars || {};
 
+//Default variables objects
+playground.defaultVariables = {
+	winHeight:	$(window).height(),
+	scrollElem:	$('#scrollElement li, .inside-container'),
+	win:		$(window)
+
+};
+// resize the containers to the size of the browser height
+playground.containers = {
+	init:function(){
+		var opt = playground.defaultVariables;
+		(function(){
+			opt.scrollElem.height(opt.winHeight);
+			opt.win.resize(function(){
+				var h = $(window).height();
+				$('#scrollElement li, .inside-container').height(h);
+			});
+		})();
+	}
+};
+//Initlize all plugins and scrips here
+$(document).ready(function(){
+    playground.containers.init();
+});
